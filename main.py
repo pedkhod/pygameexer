00bc0a187c41192236e98185b3f562e9b58ad4d0
pedkhod <pkhoddami@gmail.com>
2021-04-29 02:32:50 +0430

    title

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ pygame.init
 
 #main window
 screen = pygame.display.set_mode((800,600))
-
+#title and icon
+pygame.display.set_caption("q q fazai")
+icon=pygame.image.load("123.gif")
+pygame.display.set_icon(icon)
 #game loop
 ruuing = True
 while ruuing:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
          ruuing=False
-
+    #background color
+    screen.fill((255,0,0))
+    
+    pygame.display.update()
