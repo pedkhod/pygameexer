e101cbcd988d32db19599222a92dcdc064973e16
pedkhod <pkhoddami@gmail.com>
2021-05-03 17:10:26 +0430

    missile fire

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #import 
 import pygame
-
+import math
+import random
 
 #init pygame
 pygame.init
 
 #main window
 screen = pygame.display.set_mode((800,600))
+background=pygame.image.load('b.png')
 #title and icon
 pygame.display.set_caption("q q fazai")
 icon=pygame.image.load("123.gif")
@@ -16,13 +18,22 @@ playerX_change=0
 playerImg=pygame.image.load('spaceship.png')
 playerX=370
 playerY=540
-def player(playerX,playerY):
-    screen.blit(playerImg,(playerX,playerY))
+#miisle
+missileY_change=15
+missileImg=pygame.image.load('missile.png')
+missileX=playerX
+missileY=playerY
+missile_state='ready'
+def fire_missile(missileX,missileY):
+    global missile_state
+    missile_state='fire'
+    screen.blit(missileImg,(missileX-4,missileY-30))
 #enemy
-enemyX_change=0
+enemyX_change=3
+enemyY_change=15
 enemyImg=pygame.image.load('enemy.png')
-enemyX=370
-enemyY=50
+enemyX=random.randint(0,800)
+enemyY=random.randint(25, 150)
 def player(playerX,playerY):
     screen.blit(playerImg,(playerX,playerY))
 def enemy(enemyX,enemyY):
@@ -31,15 +42,19 @@ def enemy(enemyX,enemyY):
 ruuing = True
 while ruuing:
     screen.fill((0,0,0))
+    screen.blit(background, (0,0))
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
          ruuing=False
 
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:
-                playerX_change=-0.3
+                playerX_change=-3
             if event.key==pygame.K_RIGHT:
-                playerX_change=0.3
+                playerX_change=3
+            if event.key==pygame.K_SPACE:
+                fire_missile(playerX,missileY)
+                
         if event.type==pygame.KEYUP:
             if event.key==pygame.K_RIGHT or event.key==pygame.K_LEFT:
                 playerX_change=0
@@ -49,6 +64,21 @@ while ruuing:
         playerX= 0
     elif playerX>=763:
         playerX=763
+    enemyX+=enemyX_change
+    if enemyX<=0:
+        enemyX_change= 4
+        enemyY+=enemyY_change
+    elif enemyX>=763:
+        enemyX_change=-4
+        enemyY+=enemyY_change
+    
+    if missileY<=0 :
+        missileY=540
+        missile_state='ready'
+    if missile_state is 'fire':
+        
+        fire_missile(playerX, missileY)
+        missileY-=missileY_change
     player(playerX,playerY)
     enemy(enemyX, enemyY)
 
